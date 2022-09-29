c8f8d918f87cec4c969a7321345d1d887cb302be
Max Herrera <harvey.herrera@lumation.co>
2022-09-29 17:54:49 -0600

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Max Herrera on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
