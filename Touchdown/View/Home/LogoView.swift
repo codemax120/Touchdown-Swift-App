e6257d0a69ac0cf2a0482a8d205b0b202cb0748e
Max Herrera <harvey.herrera@lumation.co>
2022-09-29 19:19:16 -0600

    NavigationBarView is added

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,38 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Max Herrera on 9/29/22.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    var body: some View {
+        HStack {
+
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
